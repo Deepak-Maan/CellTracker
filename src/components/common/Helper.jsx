65e535b5fc5e4aed214dd79f633c5441226b4d60
Deepak-Maan <jattdevta1008@gmail.com>
2024-06-04 11:59:38 +0530

    code merged

--- src/components/common/Helper.jsx
+++ src/components/common/Helper.jsx
@@ -1,14 +1,3 @@
-export const Advantage = [
-    {
-      icon: <Money />,
-      activeIcon: <MoneyActive />,
-      title: "Money Magicians",
-      discription:
-        "Pulvinar scelerisque viverra at donec nunc orci ullam corper penatibus crasigula praesent dictum fames suscipit nam. Fermentum praesent vel adipiscing eget dictum. Risus id tellus .",
-      // TODO use  one  icon for active  or not active state
-    },
-  
-  ];
 import { InstallIcon, NetworkIcon, PrivateIcon, PuzzleIcon } from "./Icons";
 
 export const PrivaryData = [
