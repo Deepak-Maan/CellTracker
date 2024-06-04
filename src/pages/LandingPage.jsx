38ef7352385ca4c43ea312fcc92ff05794441890
Deepak-Maan <jattdevta1008@gmail.com>
2024-06-04 10:08:39 +0530

    modified Landing footer

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import LandingFooter from '../components/LandingFooter'
 
 const LandingPage = () => {
     return (
         <div>
-
+            <LandingFooter />
         </div>
     )
 }
