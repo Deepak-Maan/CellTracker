3797078d0f638bae2d35920930055977e6cd3ee5
Deepak-Maan <jattdevta1008@gmail.com>
2024-06-05 10:30:27 +0530

    modified landingHeader

--- src/components/landing/LandingFooter.jsx
+++ src/components/landing/LandingFooter.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
-import { Facebook, Instagram, Linkedin, Logo, Twitter } from '../common/Icons'
+import { Facebook, FooterLogo, Instagram, Linkedin, Twitter } from '../common/Icons'
 
 const LandingFooter = () => {
     return (
         <div className='container xl:max-w-[1164px] px-3 pt-[52px] sm:pt-[60px] lg:pt-[102px]'>
             <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 items-center pb-[58px] sm:pb-12 border-b border-darkBlue justify-between gap-[38px] lg:gap-0">
                 <div className='flex justify-center lg:justify-start'>
-                    <Logo />
+                    <FooterLogo />
                 </div>
                 <div>
                     <ul className='flex gap-6 justify-center '>
--- src/components/landing/LandingHero.jsx
+++ src/components/landing/LandingHero.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import mobileImg from "../../assets/images/landingPage/headerMobileImg.png"
-import { Location, LocationEllipse, Logo } from '../common/Icons'
+import { Location, LocationEllipse, Logo, Logo2 } from '../common/Icons'
 import { navData } from '../common/Helper'
 const LandingHero = () => {
     const [navBar, setnavBar] = useState(false);
@@ -14,19 +14,16 @@ const LandingHero = () => {
     }
     return (
         <div>
-            <div className="container mx-auto px-3 pt-[83px] max-w-[1164px]">
+            <div className="container mx-auto px-3 max-w-[1164px]">
                 <nav>
                     <div className="max-w-[1164px] px-6 sm:px-3 mx-auto pt-[27px]">
                         <div className="flex justify-between items-center">
                             <a href="/" className="relative z-[4]" aria-label="logo">
-                                {navBar ? <Logo /> : <Logo />}
+                                {navBar ? <Logo2 /> : <Logo />}
                             </a>
-                            <ul
-                                className={`flex items-center gap-[35px] duration-300 max-md:fixed max-md:w-full max-md:h-full max-md:justify-start max-md:pt-[121px] max-md:flex-col max-md:z-[3] max-md:top-0 max-md:left-[-105%] max-md:p-[30px] max-md:bg-white ${navBar && "!left-0"
-                                    }`}
-                            >
-                                {navData.map((obj, i) => (
-                                    <li key={i}>
+                            <ul className={`flex items-center gap-[35px] duration-300 max-md:fixed max-md:w-full max-md:h-full max-md:justify-start max-md:pt-[121px] max-md:flex-col max-md:z-[3] max-md:top-0 max-md:left-[-105%] max-md:p-[30px] max-md:bg-white ${navBar && "!left-0"}`} >
+                                {navData.map((obj, index) => (
+                                    <li key={index}>
                                         <a
                                             href="/"
                                             className="text-black md:text-white  text-base leading-[26.59px] font-normal relative z-[1] after:w-0 after:bg-white after:h-[2px] after:bottom-1 after:left-[50%] after:duration-300 after:rounded-md after:absolute hover:after:left-0 hover:after:w-full"
@@ -35,18 +32,18 @@ const LandingHero = () => {
                                         </a>
                                     </li>
                                 ))}
-                                <button className=" md:hidden bg-dark-blue text-base leading-[26.59px] font-semibold text-white py-4 px-7 rounded-[4px] hover:text-dark-blue hover:bg-white duration-300 relative border-transparent border-2 w-full hover:border-dark-blue z-[1] after:absolute after:h-full after:w-1 after:top-0 after:skew-x-12 after:blur-[3px] overflow-hidden after:left-[-20%] after:bg-dark-blue hover:after:animate-moveToX">
+                                <button className=" md:hidden bg-dark-blue text-base leading-7 bg-darkBlue font-semibold text-white py-4 px-7 rounded-[4px] hover:text-dark-blue hover:bg-darkBlue duration-300  border-transparent border-2 w-full hover:border-darkBlue ">
                                     Log In
                                 </button>
                             </ul>
-                            <button className=" text-dark-blue max-md:hidden text-base leading-[26.59px] font-semibold bg-white py-[14px] md:py-4 px-7 rounded-[4px] hover:bg-dark-blue hover:text-white duration-300 relative z-[1] after:absolute after:h-full after:w-1 after:top-0 after:skew-x-12 after:blur-[3px] overflow-hidden after:left-[-20%] after:bg-white hover:after:animate-moveToX">
+                            <button className=" text-darkBlue max-md:hidden text-base leading-7 font-semibold bg-white py-[14px] md:py-4 px-7 rounded-[4px] hover:bg-darkBlue hover:text-white duration-300">
                                 Log In
                             </button>
                             <button onClick={showNav} className="md:hidden relative z-[4]">
                                 {navBar ? (
                                     <div className="flex md:hidden flex-col relative z-50 justify-between w-[18px] h-[17px] cursor-pointer">
-                                        <span className="bg-black w-[11px] h-[2px] relative z-50 transition-all ease-linear duration-300 rotate-[45deg] translate-x-0 translate-y-0 -bottom-[11px]"></span>
-                                        <span className="bg-black w-[11px] h-[2px] relative z-50  transition-all ease-linear duration-300 rotate-[-45deg] translate-x-0 translate-y-1 bottom-[8px]"></span>
+                                        <span className="bg-black w-[15px] h-[1.5px] relative z-50 transition-all ease-linear duration-300 rotate-[45deg] translate-x-0 translate-y-0 -bottom-[11px]"></span>
+                                        <span className="bg-black w-[15px] h-[1.5px] relative z-50  transition-all ease-linear duration-300 rotate-[-45deg] translate-x-0 translate-y-1 bottom-[8px]"></span>
                                     </div>
                                 ) : (
                                     <div className="flex md:hidden flex-col z-50 justify-between w-[24px] h-[24px] cursor-pointer relative">
@@ -59,8 +56,8 @@ const LandingHero = () => {
                         </div>
                     </div>
                 </nav>
-                <div className="grid grid-cols-1 xl:grid-cols-2">
-                    <div className='pt-[71px]'>
+                <div className="grid grid-cols-1 xl:grid-cols-2 pt-14 pb-[76px]  md:pt-[83px]">
+                    <div className='md:pt-[71px]'>
                         <h1 className='text-xxl leading-xxl font-extrabold text-white'><span className="xl:text-nowrap">Locate any phone,</span> <span className="font-bold xl:text-nowrap">anywhere, anytime</span></h1>
                         <p className='text-base font-normal leading-6 text-white pt-2'>Lorem ipsum dolor sit amet consectetur. Nisl vitae arcu volutpat nunc cursus. Lacus ornare aliquet condimentum elementum elementum nisl nibh sed. Egestas aliquam id quam nunc.</p>
                     </div>
