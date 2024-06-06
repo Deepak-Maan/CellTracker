82f1918d691eb416259d5d0be3f85a735d99ccff
Deepak-Maan <jattdevta1008@gmail.com>
2024-06-06 09:41:17 +0530

    added ellipse in landing page

--- src/components/landing/Privacy.jsx
+++ src/components/landing/Privacy.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
 import { PrivaryData } from '../common/Helper'
+import { LocationEllp } from '../common/Icons'
 
 const Privacy = () => {
     return (
-        <div>
+        <div className='relative overflow-hidden'>
+            <div className='absolute right-0 top-[2%] md:top-[15%] rotate-180'>
+                <LocationEllp />
+            </div>
             <div className="container max-w-[1164px] mx-auto px-3 pt-[76px] ">
                 <h2 className=' text-[28px] md:text-4xl lg:text-5xl leading-8 md:leading-10 lg:leading-xl1 font-light text-darkBlue text-center  pb-2'>Universal<span className=" font-extrabold"> Privacy</span></h2>
                 <p className=' text-sm sm:text-base font-normal leading-5 sm:leading-6 max-w-[472px] text-darkGray pb-[34px] sm:pb-14 mx-auto text-center'>Fames tincidunt pellentesque nisl blandit at sit. Ullamcorper nisi sit elit rhoncus vestibulum fames libero id.</p>
